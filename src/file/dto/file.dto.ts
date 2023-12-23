978ad710feefd8332121c1be163eacb4828058af
fabian jiron <61808329+fabjiro@users.noreply.github.com>
2023-12-23 16:23:27 -0600

    Remove verifi is base64string

--- src/file/dto/file.dto.ts
+++ src/file/dto/file.dto.ts
@@ -1,9 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBase64, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class FileCreateDto {
   @ApiProperty()
   @IsNotEmpty({ message: 'El campo file es requerido' })
-  @IsBase64({ message: 'El campo file debe ser base64' })
   file: string;
 }
